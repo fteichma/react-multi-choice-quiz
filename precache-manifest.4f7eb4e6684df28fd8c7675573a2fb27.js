ff08832e7f7b0a8b681bbd42d0d5c71ff3304866
fteichma <fteichma@MacBook-Pro.lan>
2020-07-23 01:37:13 +0200

    Updates

---
+++ precache-manifest.4f7eb4e6684df28fd8c7675573a2fb27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45efa812d41e254681ae9b54da5b12d",
+    "url": "/react-multi-choice-quiz/index.html"
+  },
+  {
+    "revision": "40e9c53e8bf11c991e8f",
+    "url": "/react-multi-choice-quiz/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "7004f95b9350d58eca98",
+    "url": "/react-multi-choice-quiz/static/css/main.6137dafc.chunk.css"
+  },
+  {
+    "revision": "40e9c53e8bf11c991e8f",
+    "url": "/react-multi-choice-quiz/static/js/2.6861e55b.chunk.js"
+  },
+  {
+    "revision": "b2490fbb34248c3403116f401942d851",
+    "url": "/react-multi-choice-quiz/static/js/2.6861e55b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7004f95b9350d58eca98",
+    "url": "/react-multi-choice-quiz/static/js/main.dcbfdf41.chunk.js"
+  },
+  {
+    "revision": "23146cc76138630b0b1e",
+    "url": "/react-multi-choice-quiz/static/js/runtime-main.fd91f373.js"
+  }
+]);
