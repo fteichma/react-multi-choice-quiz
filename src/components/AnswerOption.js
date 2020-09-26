e1a040ceeffc48556e5e71a86331f65bcf34320f
fteichma <fteichma@MacBook-Pro.lan>
2020-09-26 12:04:07 +0200

    Poppins, answerOptions

--- src/components/AnswerOption.js
+++ src/components/AnswerOption.js
@@ -7,54 +7,28 @@ import 'animate.css';
 function AnswerOption(props) {
     switch(props.answerType) {
       case 'text' :
+      case 'email' :
+      case 'number' :
         return (
           <>
           <li className="answerOption">
             <input
             className={props.error ? `animate__animated animate__shakeX` : ``} 
-            type="text" 
+            type={props.answerType} 
             placeholder={props.answerContent} 
             onKeyPress={props.onKeyPressed}
+            autoFocus
             />
           </li>
-          {props.error && <span className="info-error">Veuillez remplir le champ</span>}
+          {props.error && <span className="info-error">Veuillez compléter le champ</span>}
           </>
         )
-        case 'email' :
-          return (
-            <>
-            <li className="answerOption">
-              <input 
-              className={props.error ? `animate__animated animate__shakeX` : ``}
-              type="email" 
-              placeholder={props.answerContent} 
-              onKeyPress={props.onKeyPressed}
-              />
-            </li>
-            {props.error && <span className="info-error">Veuillez remplir le champ</span>}
-            </>
-          )
           case 'body' :
             return(
               <>
               <BodyFront className="bodySelect" onClick={props.onAnswerSelected}/>
               <BodyBack className="bodySelect" onClick={props.onAnswerSelected}/>
               </>
-            )
-          case 'number' :
-            return (
-              <>
-              <li className="answerOption">
-                <input 
-                className={props.error ? `animate__animated animate__shakeX` : ``}
-                type="number" 
-                placeholder={props.answerContent} 
-                onKeyPress={props.onKeyPressed}
-                />
-              </li>
-                              {props.error && <span className="info-error">Veuillez remplir le champ</span>}
-                              </>
-
             )
       default : 
   return (
